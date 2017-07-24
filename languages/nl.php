952a96e408fb734a6c75121755ed0634c9aee868
Jerome Bakker <jeabakker@coldtrick.com>
2017-07-24 17:32:16 +0200

    chore: updated Dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,7 +1,94 @@
 <?php
 /**
  * This file was created by Translation Editor v6.0-beta
- * On 2017-07-24 16:03
+ * On 2017-07-24 17:31
  */
 
-return NULL;
+return array (
+  'item:object:idea' => 'Idee',
+  'ideation:breadcrumb:all' => 'Ideation',
+  'ideation:breadcrumb:suggested' => 'Aangeraden',
+  'ideation:add' => 'Nieuw idee',
+  'ideation:no_results' => 'Er zijn nog geen ideeën aangemaakt',
+  'ideation:settings:enable_personal' => 'Schakel Ideation in voor persoonijlk gebruik',
+  'ideation:settings:enable_personal:help' => 'Indien ingeschakeld kunnen gebruiker Ideation gebruiken in hun persoonlijke context, dus buiten groepen.',
+  'ideation:settings:enable_groups' => 'Schakel Ideation in voor groepen',
+  'ideation:settings:enable_groups:help' => 'Indien ingeschakeld kunnen groepsbeheerders Ideation inschakelen voor hun groep. Standaard is Ideation uitgeschakeld voor een groep.',
+  'ideation:settings:suggested_questions_profile_fields' => 'Welke profielvelden moeten worden gebruikt in de aangeraden vragen widget',
+  'ideation:settings:suggested_questions_profile_fields:help' => 'Configureer welke profielvelden er moeten worden gebruikt in de aangeraden vragen widegt. Meerdere profielvelden moeten worden gescheiden door een komma.',
+  'ideation:group_tool_option:label' => 'Groep ideation inschakelen',
+  'ideation:group_tool_option:description' => 'Stel groepsleden in staat om ideeën te delen en te realiseren',
+  'river:create:object:idea' => '%s heeft een nieuw idee %s',
+  'ideation:river:object:question:attachment' => 'Gesteld bij het idee: %s',
+  'ideation:menu:owner_block:groups' => 'Groep ideation',
+  'ideation:menu:site:all' => 'Ideation',
+  'ideation:menu:filter:suggested' => 'Aangeraden',
+  'ideation:all:title' => 'Alle ideeën',
+  'ideation:owner:title' => '%s\'s ideeën',
+  'ideation:owner:title:mine' => 'Mijn ideeën',
+  'ideation:friends:title' => 'Ideeën van vrienden',
+  'ideation:friends:title:mine' => 'Ideeën van mijn vrienden',
+  'ideation:group:title' => '%s\'s ideeën',
+  'ideation:add:title' => 'Nieuw idee',
+  'ideation:edit:title' => 'Bewerk idee: %s',
+  'ideation:suggested:title' => 'Aangeraden vragen voor jou',
+  'ideation:suggested:error:profile_fields' => 'Er zijn geen profielvelden geconfigureerd om interessante vragen voor jou te tonen, neem contact op met je site beheerder',
+  'ideation:suggested:error:logged_out' => 'Je moet zijn aangemeld om deze lijst te kunnen bekijken',
+  'ideation:suggested:error:user_profile' => 'Er is niet genoeg informatie gevonden op je profiel om vragen aan te kunnen raden, vul je profiel aan.',
+  'ideation:suggested:error:no_results' => 'Er konden geen vragen worden gevonden op basis van je profiel informatie.',
+  'ideation:edit:icon' => 'Icoon',
+  'ideation:edit:icon:limit' => 'Maximaal toegestane bestandsgrootte is %s',
+  'ideation:edit:icon:remove' => 'Verwijder het huidige icoon',
+  'ideation:edit:icon:remove:help' => 'Verwijderd het huidige icoon, dit heeft voorrang boven het uploaden van een nieuw icoon',
+  'ideation:status:new' => 'Nieuw',
+  'ideation:status:accepted' => 'Geaccepteerd',
+  'ideation:status:rejected' => 'Afgewezen',
+  'ideation:status:implemented' => 'Geïmplementeerd',
+  'ideation:notification:create:subject' => 'Er is een nieuw idee aangemaakt: %s',
+  'ideation:notification:create:summary' => 'Beste %s,
+
+%s heeft een nieuw idee aangemaakt \'%s\'. Help bij het realiseren van dit idee.
+
+Om het idee te bekijken, klik hier:
+%s',
+  'ideation:notification:idea:owner:question:create:subject' => 'Er is een vraag gesteld op je idee: %s',
+  'ideation:notification:idea:owner:question:create:summary' => 'Beste %s,
+
+%s heeft een vraag gesteld op jouw idee \'%s\'.
+
+%s
+
+Om het idee te bekijken, klik hier:
+%s
+
+Om de vraag te bekijken, klik hier:
+%s',
+  'ideation:notification:idea:owner:answer:create:subject' => 'Er is een antwoord gegeven voor jou idee: %s',
+  'ideation:notification:idea:owner:answer:create:summary' => 'Beste %s,
+
+%s heeft een antwoord gegeven op de vraag \'%s\' voor jouw idee \'%s\'.
+
+%s
+
+Om het idee te bekijken, klik hier:
+%s
+
+Om het antwoord te bekijken, klik hier:
+%s',
+  'ideation:questions:edit:link' => 'Deze vraag zal worden gekoppeld aan het idee: %s',
+  'ideation:questions:related' => 'Vragen gerelateerd aan dit idee',
+  'ideation:questions:idea:title' => 'Gerelateerd idee',
+  'widgets:ideation_suggested_questions:name' => 'Aangeraden vragen',
+  'widgets:ideation_suggested_questions:description' => 'Toon een lijst van open vragen bij ideeën gebaseerd op profiel informatie van de gebruiker',
+  'widgets:ideation:name' => 'Ideeën',
+  'widgets:ideation:description' => 'Toon een lijst van ideeën met verschillende selectie mogelijkheden',
+  'widgets:ideation:status' => 'Status van het idee',
+  'widgets:ideation:sort:updated' => 'Recent bijgewerkt',
+  'widgets:ideation:listing' => 'Lijst weergave',
+  'widgets:ideation:listing:default' => 'Standaard content weergave',
+  'widgets:ideation:listing:image' => 'Weergave met afbeelding',
+  'ideation:action:edit:success' => 'Je idee is succesvol opgeslagen',
+  'ideation:target_audience' => 'Doelgroep',
+  'ideation:problem' => 'Dit probleem wordt opgelost',
+  'ideation:problem:question' => 'Voor welk probleem is dit een oplossing',
+);
